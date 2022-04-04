cf5c53be81213bed6e47004c1e4382ad16acb1fb
Jens Peder Meldgaard <jens.peder.meldgaard@velux.com>
2022-04-04 19:00:15 +0200

    update ssl in postgres config

--- apps/backend/config/database.js
+++ apps/backend/config/database.js
@@ -7,7 +7,9 @@ module.exports = ({ env }) => ({
       database: env('DATABASE_NAME', 'app'),
       user: env('DATABASE_USERNAME', ''),
       password: env('DATABASE_PASSWORD', ''),
-      ssl: env.bool('DATABASE_SSL', false),
+      ssl: {
+        rejectUnauthorized: env.bool('DATABASE_SSL_SELF', false), // For self-signed certificates
+      },
     },
   },
 });
